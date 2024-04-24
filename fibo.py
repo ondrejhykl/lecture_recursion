76ac2bbe23259e19d484b7fc3e4e44b6acf2d0dc
Ondřej Hykl <ondra.hykl@seznam.cz>
2024-04-24 09:11:19 +0200

    Add fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,12 @@
+def recursive_nth_fibo(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
-    pass
+    print(recursive_nth_fibo(10))
 
 
 if __name__ == "__main__":
